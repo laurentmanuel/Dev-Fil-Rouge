f1a00d02eb7a1d41c1bcc173d4f2976a673954c6
laurentmanuel <lm.laurentmanuel@gmail.com>
2021-09-15 09:01:00 +0200

    css navbar surlignée au survol

--- view/navbar.php
+++ view/navbar.php
@@ -1,13 +1,13 @@
 <!-- menu Navigation -->
 <nav>
   <ul>
-    <li><a href="vueNews.php">News</a></li>
-    <li><a href="vueExplorer.php">Explorer</a></li>
-    <li><a href="vueReservations.php">Réserver</a></li>
+    <li><a href="vueNews.php" class="menu__item">News</a></li>
+    <li><a href="vueExplorer.php" class="menu__item">Explorer</a></li>
+    <li><a href="vueReservations.php" class="menu__item">Réserver</a></li>
     <?php if (!isset($_SESSION["user"])) : ?>
-      <li><a href="vueLogin.php">Mon Compte</a></li>
+      <li><a href="vueLogin.php" class="menu__item">Mon Compte</a></li>
     <?php else : ?>
-      <li><a href="deconnexion.php">Se déconnecter</a></li>
+      <li><a href="deconnexion.php" class="menu__item">Se déconnecter</a></li>
     <?php endif; ?>
   </ul>
 </nav>
